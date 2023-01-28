bb66c41c0b09593d91ca3a875c72ab068f1501e9
Yadav106 <sahil.jp.yadav@gmail.com>
2023-01-28 23:15:22 +0530

    added firebase

--- wms_frontend/src/App.jsx
+++ wms_frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import {Link, Route, Routes} from 'react-router-dom'
+import Elements from './Elements'
 
 const App = () => {
   return (
     <>
       <nav>
         <ul>
-          {/* <li><Link to='/warehouse'>Warehouse</Link></li> */}
+          <li><Link to='/element'>Elements</Link></li>
         </ul>
       </nav>
       <Routes>
-        {/* <Route path='/warehouse' element={<Warehouse />}/> */}
+        <Route path='/element' element={<Elements />}/>
       </Routes>
     </>
   )
